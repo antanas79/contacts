e9e82d889b2e74bf409d2debb31eb970c2b7036c
antanas79 <33091920+antanas79@users.noreply.github.com>
2018-06-24 23:08:04 +0300

    Update contact.js
    deleting comments

--- contact.js
+++ contact.js
@@ -44,40 +44,6 @@ const questions = [
   },
 
 ];
-db.createCollection("contacts", {
-   validator: {
-      $jsonSchema: {
-         bsonType: "object",
-         required: [ "firstname", "lastname", "email", "phonenumber1", "phonenumber2", "comment" ],
-         properties: {
-            firstname: {
-               bsonType: "string",
-               description: "must be a string and is required"
-            },
-            // gender: {
-            //    bsonType: "string",
-            //    description: "must be a string and is not required"
-            // },
-            // year: {
-            //    bsonType: "int",
-            //    minimum: 2017,
-            //    maximum: 3017,
-            //    exclusiveMaximum: false,
-            //    description: "must be an integer in [ 2017, 3017 ] and is required"
-            // },
-            // major: {
-            //    enum: [ "Math", "English", "Computer Science", "History", null ],
-            //    description: "can only be one of the enum values and is required"
-            // },
-            // gpa: {
-            //    bsonType: [ "double" ],
-            //    minimum: 0,
-            //    description: "must be a double and is required"
-            // }
-         }
-      }
-   }
-});
 
 program
   .version('0.0.1')
